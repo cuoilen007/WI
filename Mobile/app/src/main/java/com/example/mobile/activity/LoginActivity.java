f31413b73f6a6554db7cefe5647dc2ea9604d97a
vinhvizg <vinhvizg1040@gmail.com>
2022-08-10 14:13:45 +0700

    layout-login

---
+++ Mobile/app/src/main/java/com/example/mobile/activity/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.mobile.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.mobile.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView tvRegister = findViewById(R.id.a_tv_register);
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
