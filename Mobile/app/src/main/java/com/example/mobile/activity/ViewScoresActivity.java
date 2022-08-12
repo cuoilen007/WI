26446d763f1a4d8bec544febe7301f070980ee7b
cuoilen007 <havy59@gmail.com>
2022-08-12 20:48:34 +0700

    ViewScore

--- Mobile/app/src/main/java/com/example/mobile/activity/ViewScoresActivity.java
+++ Mobile/app/src/main/java/com/example/mobile/activity/ViewScoresActivity.java
@@ -1,18 +1,36 @@
 package com.example.mobile.activity;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mobile.R;
+import com.example.mobile.fkfirebase.FkFireBase;
+import com.example.mobile.model.ScoreDetails;
+import com.example.mobile.model.Subject;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ViewScoresActivity extends AppCompatActivity {
+    List<Subject> subjectList = new ArrayList<>();
+    List<ScoreDetails> scoreDetailsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +40,99 @@ public class ViewScoresActivity extends AppCompatActivity {
         Spinner spSubject = findViewById(R.id.spSubject);
         TextView tvName = findViewById(R.id.tvName);
         TextView tvClass = findViewById(R.id.tvClass);
-        TextView tvSemester = findViewById(R.id.tvSemester);
+        TextView tvFastTest1 = findViewById(R.id.tvFastTest1);
+        TextView tvMiddleTest1 = findViewById(R.id.tvMiddleTest1);
+        TextView tvFinalTest1 = findViewById(R.id.tvFinalTest1);
+        TextView tvTotal1 = findViewById(R.id.tvTotal1);
+        TextView tvFastTest2 = findViewById(R.id.tvFastTest2);
+        TextView tvMiddleTest2 = findViewById(R.id.tvMiddleTest2);
+        TextView tvFinalTest2 = findViewById(R.id.tvFinalTest2);
+        TextView tvTotal2 = findViewById(R.id.tvTotal2);
+
 
         List<String> subjectAdapter = new ArrayList<String>() {
-            {
-                add("Math");
-                add("English");
-                add("Chemistry");
-                add("Physical");
-            }
         };
+        subjectAdapter.add("Select Subject");
+
+        FirebaseFirestore db1 = FirebaseFirestore.getInstance();
+        db1.collection("Subject")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                subjectAdapter.add(document.toObject(Subject.class).getSubjectName());
+                                subjectList.add(document.toObject(Subject.class));
+                            }
+
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
         spSubject.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, subjectAdapter));
         tvName.setText("John Smith");
         tvClass.setText("12A");
-        tvSemester.setText("1");
+
+
+
+
+        spSubject.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                FirebaseFirestore db2 = FirebaseFirestore.getInstance();
+                db2.collection("ScoreDetails")
+                        .whereEqualTo("SubjectName", spSubject.getSelectedItem().toString())
+                        .whereEqualTo("studentid","2")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        scoreDetailsList.add(document.toObject(ScoreDetails.class));
+                                    }
+
+                                    for(ScoreDetails score : scoreDetailsList){
+                                        if(score.getTestname().equals("fast-test1")){
+                                            tvFastTest1.setText(String.valueOf(score.getScoreReceived()));
+                                        }
+                                        if(score.getTestname().equals("middle-test1")){
+                                            tvMiddleTest1.setText(String.valueOf(score.getScoreReceived()));
+                                        }
+                                        if(score.getTestname().equals("final-test1")){
+                                            tvFinalTest1.setText(String.valueOf(score.getScoreReceived()));
+                                        }
+                                        if(score.getTestname().equals("fast-test2")){
+                                            tvFastTest2.setText(String.valueOf(score.getScoreReceived()));
+                                        }
+                                        if(score.getTestname().equals("middle-test2")){
+                                            tvMiddleTest2.setText(String.valueOf(score.getScoreReceived()));
+                                        }
+                                        if(score.getTestname().equals("final-test2")){
+                                            tvFinalTest2.setText(String.valueOf(score.getScoreReceived()));
+                                        }
+                                    }
+
+                                } else {
+                                    Log.w(TAG, "Error getting documents.", task.getException());
+                                }
+                            }
+                        });
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        //chart
+
+
     }
+
 }
