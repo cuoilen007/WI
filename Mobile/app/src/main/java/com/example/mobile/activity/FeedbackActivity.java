1faf0b1ffd2ca5f9fa5cada5901eb845a73a090a
Oka Ayuko <oka.ayuko0210@gmail.com>
2022-08-13 16:20:25 +0700

    Update Layout 13/08/22

--- Mobile/app/src/main/java/com/example/mobile/activity/FeedbackActivity.java
+++ Mobile/app/src/main/java/com/example/mobile/activity/FeedbackActivity.java
@@ -16,14 +16,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.mobile.R;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class FeedbackActivity extends AppCompatActivity {
